a59128c3d8c4c2a291fc785d7ade496058cb69c1
ramagantengrek-afk <ramagantengrek@gmail.com>
2026-06-15 13:57:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ TOKEN = os.getenv("DISCORD_TOKEN", "YOUR_DISCORD_BOT_TOKEN")
 DB_PATH = "pokemon_bot.db"
 
 # Bot Settings
-DEFAULT_PREFIX = "p!"
+DEFAULT_PREFIX = "/"
 
 # API Settings
 POKEAPI_BASE = "https://pokeapi.co/api/v2/"
